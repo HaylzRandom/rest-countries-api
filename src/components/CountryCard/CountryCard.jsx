8818d79ba4542fcedebfae220d9d44fc80f1756e
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-11-22 15:39:19 +0000

    Added mobile styling for CountryList, SearchBar and CountryFilter components

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -8,26 +8,24 @@ import './countryCard.css';
 const CountryCard = () => {
 	return (
 		<>
-			<Link>
-				<div className='card'>
-					<div className='card__image'>
-						<img src={flagImage} alt='' />
-					</div>
-
-					<div className='card__data'>
-						<h2 className='card__data--title'>Germany</h2>
-						<p className='card__data--info'>
-							<span className='strong'>Population: </span>323,947,000
-						</p>
-						<p className='card__data--info'>
-							<span className='strong'>Region: </span>Europe
-						</p>
-						<p className='card__data--info'>
-							<span className='strong'>Capital: </span>Berlin
-						</p>
-					</div>
+			<div className='card'>
+				<div className='card__image'>
+					<img src={flagImage} alt='' />
 				</div>
-			</Link>
+
+				<div className='card__data'>
+					<h2 className='card__data--title'>Germany</h2>
+					<p className='card__data--info'>
+						<span className='strong'>Population: </span>323,947,000
+					</p>
+					<p className='card__data--info'>
+						<span className='strong'>Region: </span>Europe
+					</p>
+					<p className='card__data--info'>
+						<span className='strong'>Capital: </span>Berlin
+					</p>
+				</div>
+			</div>
 		</>
 	);
 };
